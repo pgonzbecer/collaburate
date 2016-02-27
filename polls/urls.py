e3e5154a00989027faabae815ca2e5a0ca495937
Paul Gonzalez-Becerra <pgonzbecer@gmail.com>
2016-02-27 04:41:37 +0000

    Part 4

--- polls/urls.py
+++ polls/urls.py
@@ -4,8 +4,13 @@ from django.conf.urls import url
 
 from . import views
 
+app_name=   "polls"
+
 urlpatterns=    [
-    url(r"^$", views.index, name="index")
+    url(r"^$", views.index, name="index"), # /polls/
+    url(r"^(?P<question_id>[0-9]+)/$", views.detail, name="detail"), # /polls/5/
+    url(r"^(?P<question_id>[0-9]+)/results/$", views.results, name="results"), # /polls/5/results
+    url(r"^(?P<question_id>[0-9]+)/vote/$", views.vote, name="vote") # /polls/5/vote/
 ]
 
 # End of File
